76d4782dd65f9ca93d871d185f93d56d39fcee34
xiehuanyun2018302110069 <2018302110069@whu.edu.cn>
2021-09-27 13:14:59 +0800

    完善项目结构

---
+++ api/url.py
@@ -0,0 +1,11 @@
+"""
+@author:azi
+@file:url.py
+@time:2021/9/26
+"""
+
+
+from retuo import app
+
+
+#app.add_url_rule('/auth/login',view_func=)
--- config.py
+++ config.py
@@ -1,9 +1,25 @@
 # _*_ coding=utf-8 _*_
-class BaseConfig(object):
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig:  # 基本配置类
     HOST = '127.0.0.1'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dddd_retuo')
+    ITEMS_PER_PAGE = 10
 
 
-class DevConfig(BaseConfig):
+class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://mysql:MySql337618:@127.0.0.1:3306/'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:NySQL337618@127.0.0.1/retuo'
 
+
+class TestingConfig(BaseConfig):
+    TESTING = TrueWTF_CSRF_ENABLED = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'test': TestingConfig,
+    'default': DevelopmentConfig
+}
